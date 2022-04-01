60f4e73018ce36c75675a7fc8701f073ccdf3112
SN-Beaux <Beaux.Luo@supernovacompanies.biz>
2022-04-01 06:36:34 +0000

    Update at 2022-04-01T06:36:34.023188Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,12 +2,12 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        System.out.println(multiplyBy31(0));
-        System.out.println(multiplyBy31(1));
-        System.out.println(multiplyBy31(5));
-        System.out.println(divideBy2ThenMinus1(1));
-        System.out.println(divideBy2ThenMinus1(4));
-        System.out.println(divideBy2ThenMinus1(7));
+        // System.out.println(multiplyBy31(0));
+        System.out.println(multiplyBy31(-1));
+        System.out.println(multiplyBy31(-5));
+        // System.out.println(divideBy2ThenMinus1(1));
+        // System.out.println(divideBy2ThenMinus1(4));
+        // System.out.println(divideBy2ThenMinus1(7));
     }
 
     /**
@@ -16,8 +16,34 @@ public class Challenge {
      * you are not allowed to use multiplication sign (`*`), division sign (`/`), and addition sign
      * (`+`).
      */
-    public static int multiplyBy31(int n) {
-        return 0;
+    public static int multiplyBy31(int b) {
+        boolean minus = false;
+        if (b < 0) {
+            minus = true;
+            b = Math.abs(b);
+        }
+        int base = 31;
+        int i = 0;
+        int res = 0;
+        //乘数不为0
+        while (b != 0) {
+            //处理当前位
+            //当前位是1
+            if ((b & 1) == 1) {
+                res += (base << i);
+                b = b >> 1;
+                //记录当前是第几位
+                i++;
+            } else {
+                //当前位是0
+                b = b >> 1;
+                i++;
+            }
+        }
+        if (minus) {
+            return add(~res, 1);
+        }
+        return res;
     }
 
     /**
@@ -32,6 +58,40 @@ public class Challenge {
      * addition sign (`+`).
      */
     public static int divideBy2ThenMinus1(int n) {
-        return 0;
+        return subtraction(division(n, 2), 1);
+    }
+
+    private static int division(int a, int b) {
+        boolean minus = false;
+        if (a < 0) {
+            minus = true;
+            a = Math.abs(a);
+        }
+        int res;
+        if (a < b) {
+            return 0;
+        } else {
+            res = division(subtraction(a, b), b) + 1;
+        }
+        if (minus) {
+            return add(~res, 1);
+        }
+        return res;
+    }
+
+    private static int subtraction(int a, int b) {
+        b = ~b + 1;
+        return add(a, b);
+    }
+
+    private static int add(int a, int b) {
+        //进位值
+        int carry;
+        while (b != 0) {
+            carry = (a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        return a;
     }
 }
