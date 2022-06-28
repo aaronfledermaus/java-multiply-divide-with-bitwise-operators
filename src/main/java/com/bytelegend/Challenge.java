27a8d3110424f6b39ff517e0098d15ffcc4f9b71
Yeely0162 <Yeely01621@163.com>
2022-06-28 01:12:37 +0000

    Update at 2022-06-28T01:12:37.496357Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -10,105 +10,13 @@ public class Challenge {
         System.out.println(divideBy2ThenMinus1(7));
     }
 
-    public static int add(int a, int b) {
-        int sum = a;
-        while (b != 0) {
-            sum = a ^ b;    //将a，b两值不带进位相加
-            b = (a & b) << 1; //b更新为进位的值
-            a = sum;   //a更新为不带进位的相加值
-        }
-        return sum;
-    }
-
-
-    /**
-     * `multiplyBy31Description(int n)` returns the result of a given integer multiplying by 31,
-     * e.g. `n=1`, return `31`; `n=2`, return `62`. No need to consider overflow issue. Note that
-     * you are not allowed to use multiplication sign (`*`), division sign (`/`), and addition sign
-     * (`+`).
-     */
-
+    
     public static int multiplyBy31(int n) {
-        return multi(n, 31);
+        int m = Math.abs(n);
+        return n < 0 ? ~((m << 5) - m - 1) : (m << 5) - m;
     }
-
-    //乘法
-    private static int multi(int a, int b) {
-        //将乘数和被乘数都取绝对值　
-        int multiplicand = a < 0 ? add(~a, 1) : a;
-        int multiplier = b < 0 ? add(~b, 1) : b;
-
-        int res = 0;
-        // 判断multiplier 任何数*0=0
-        while (multiplier != 0) {
-            //判断 multiplier 是不是 奇数
-            if ((multiplier & 1) != 0) {
-                // 如果是奇数 则加上一次multiplicand本身
-                res = add(res, multiplicand);
-            }
-            // multiplicand * 2
-            multiplicand <<= 1;
-            // multiplier  / 2
-            multiplier >>>= 1;
-        }
-        //计算乘积的符号　　
-        if ((a ^ b) < 0) {
-            res = add(~res, 1);
-        }
-        return res;
-    }
-
-    public static int divide(int a, int b) {
-        int z = b;
-        int q = a;
-        boolean isAllNeg = isNegtive(q) || isNegtive(z);
-        if (isNegtive(q))
-            q = add(~q, 1);
-        if (isNegtive(z))
-            z = add(~z, 1);
-        int ans = 0;
-        int temp;
-
-        if (q < z) {
-            return 0;
-        } else if (q == z) {
-            return 1;
-        } else {
-            for (int i = 31; i >= 0; i--) {
-                temp = q >>> i;
-                if (temp >= b) {
-                    ans += (1 << i);
-                    q -= (z << i);
-                }
-            }
-        }
-        return isAllNeg ? add(~ans, 1) : ans;
-    }
-
-    public static boolean isNegtive(int num) {
-        return num < 0;
-    }
-
-    /**
-     * 减法
-     */
-    private static int Minus(int a, int b) {
-        return add(a, add(~b, 1));
-    }
-
-    /**
-     * `divideBy2ThenMinus1(int n)` returns the result of a given integer dividing by 2, then minus
-     * 1.
-     *
-     * <p>For example, `n=1`, return `-1`, because `1/2=0`, `0-1=-1`.
-     *
-     * <p>`n=4`, return 1, because `4/2=2`, `2-1=1`.
-     *
-     * <p>Note that you are not allowed to use multiplication sign (`*`), division sign (`/`), and
-     * addition sign (`+`).
-     */
     public static int divideBy2ThenMinus1(int n) {
-
-        return Minus(divide(n, 2), 1);
+        int m = Math.abs(n);
+        return n == 0 ? -1 : n < 0 ? ~(m >> 1) : (m >> 1) - 1;
     }
 }
