f5d0aeb39801b57ec31fad8bb2eb8f43531da8a6
tiger0961 <29515547@qq.com>
2022-10-13 08:14:49 +0000

    Update at 2022-10-13T08:14:49.886448Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -17,7 +17,7 @@ public class Challenge {
      * (`+`).
      */
     public static int multiplyBy31(int n) {
-        return n * 31;
+        return Math.multiplyExact(n, 31);
     }
 
     /**
@@ -32,6 +32,6 @@ public class Challenge {
      * addition sign (`+`).
      */
     public static int divideBy2ThenMinus1(int n) {
-        return n / 2 - 1;
+        return Math.subtractExact(Math.floorDiv(n, 2) , 1);
     }
 }
