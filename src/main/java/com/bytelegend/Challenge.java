be9757bff24f42c7b2c8936fddc3c68b2b00e929
tiger0961 <29515547@qq.com>
2022-10-13 08:20:36 +0000

    Update at 2022-10-13T08:20:36.080988Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -17,7 +17,7 @@ public class Challenge {
      * (`+`).
      */
     public static int multiplyBy31(int n) {
-        return Math.multiplyExact(n, 31);
+        return Math.multiplyExact(n, 31); 
     }
 
     /**
